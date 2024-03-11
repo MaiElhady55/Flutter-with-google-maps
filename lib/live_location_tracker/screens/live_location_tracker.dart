762cccc46d0d8b1df56c20e5cadca29d1b5a6239
MaiElhady55 <ganaamr858@gmail.com>
2024-03-11 23:00:32 +0200

    feat/animate camera position depend on location data get from stream

--- lib/live_location_tracker/screens/live_location_tracker.dart
+++ lib/live_location_tracker/screens/live_location_tracker.dart
@@ -12,32 +12,32 @@ class LiveLocationTrackerScreen extends StatefulWidget {
 
 class _LiveLocationTrackerScreenState extends State<LiveLocationTrackerScreen> {
   late CameraPosition initialCameraPosition;
-  late GoogleMapController googleMapController;
+  GoogleMapController? googleMapController;
   late Location location;
   @override
   void initState() {
     initialCameraPosition = const CameraPosition(
         zoom: 12, target: LatLng(31.040848110485165, 31.37790918658407));
     location = Location();
-    checkAndRequestLocationService();
+    initMyLocation();
     super.initState();
   }
 
   @override
   void dispose() {
-    googleMapController.dispose();
+    googleMapController!.dispose();
     super.dispose();
   }
 
-  //change style of Map
+//change style of Map
   void initMapStyle() async {
     String nightMapStyle = await DefaultAssetBundle.of(context)
         .loadString('assets/map_styles/night_map_style.json');
-    googleMapController.setMapStyle(nightMapStyle);
+    googleMapController!.setMapStyle(nightMapStyle);
   }
 
 //Check and Request Location Service
-  void checkAndRequestLocationService() async {
+  Future<void> checkAndRequestLocationService() async {
     bool isServiceEnabled = await location.serviceEnabled();
     if (!isServiceEnabled) {
       isServiceEnabled = await location.requestService();
@@ -48,14 +48,43 @@ class _LiveLocationTrackerScreenState extends State<LiveLocationTrackerScreen> {
     checkAndRequestLocationPermission();
   }
 
-  //Check and Request Location Permission
-  void checkAndRequestLocationPermission() async {
+//Check and Request Location Permission
+  Future<bool> checkAndRequestLocationPermission() async {
     PermissionStatus permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.deniedForever) {
+      return false;
+    }
     if (permissionStatus == PermissionStatus.denied) {
       permissionStatus = await location.requestPermission();
       if (permissionStatus != PermissionStatus.granted) {
+        return false;
         //TODO: for EX Show Error Bar
       }
+    }
+    // mean that the permisssion in the began is allaow (granted)
+    return true;
+  }
+
+//get Location Data
+  void getLocationData() {
+    location.onLocationChanged.listen((locationData) {
+      print('Location Data $locationData');
+      CameraPosition cameraPosition = CameraPosition(
+          target: LatLng(locationData.latitude!, locationData.longitude!),
+          zoom: 12);
+      googleMapController
+          ?.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    });
+  }
+
+// Init / Set / Update  My Location combine 3 method
+  void initMyLocation() async {
+    await checkAndRequestLocationService();
+    bool hasPermission = await checkAndRequestLocationPermission();
+    if (hasPermission) {
+      getLocationData();
+    } else {
+      //TDOD :what i do if i have't the permission
     }
   }
 
